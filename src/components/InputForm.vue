8a580f838512cd7b3629d7821805b223202ee2de
kuoricky22 <kuoricky22@gmail.com>
2023-01-27 22:35:27 +0800

    完成

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -3,47 +3,96 @@ import { storeToRefs } from "pinia";
 import { useEventStore } from "../stores/events.js";
 
 const eventStore = useEventStore();
-const { setEventDeatil } = eventStore;
-const { eventDetail } = storeToRefs(eventStore);
-const { Type, Name, Price, Remake } = eventDetail.value;
+const { setEventDetail, getEventDetail } = eventStore;
+const {
+  eventDetail,
+  eventPrice,
+  eventDate,
+  eventRemark,
+  eventConsumeType,
+  eventPayType,
+  eventDateDetail,
+  selectConsumeType,
+  selectPayType,
+} = storeToRefs(eventStore);
 </script>
 
 <template>
   <div>
     <div class="Input-Form">
+      <div class="Input-Box" style="width: 100%">
+        <label for="eventPayType">請選擇花費日期</label>
+        <input type="Date" v-model="eventDate" />
+      </div>
       <div class="Input-Box">
         <label for="eventConsumeType">請選擇消費種類</label>
-        <select name="eventConsumeType" id="eventConsumeType">
-          <option value="1" selected>飲食</option>
-          <option value="2">娛樂</option>
-          <option value="3">醫療</option>
-          <option value="4">繳費</option>
-          <option value="5">交通</option>
+        <select
+          name="eventConsumeType"
+          id="eventConsumeType"
+          v-model="eventConsumeType"
+        >
+          <option disabled value="">請選擇</option>
+          <option
+            v-for="consumetype in selectConsumeType"
+            :value="consumetype.val"
+          >
+            {{ consumetype.type }}
+          </option>
         </select>
       </div>
       <div class="Input-Box">
         <label for="eventPayType">請選擇花費方式</label>
-        <select name="eventPayType" id="eventPayType">
-          <option value="Cash" selected>現金</option>
-          <option value="CreditCard">信用卡</option>
-          <option value="EasyCard">悠遊卡</option>
+        <select name="eventPayType" id="eventPayType" v-model="eventPayType">
+          <option disabled value="">請選擇</option>
+          <option v-for="payType in selectPayType" :value="payType.type">
+            {{ payType.type }}
+          </option>
         </select>
       </div>
       <div class="Input-Box" style="width: 100%">
         <label for="eventPrcie">金額</label
-        ><input id="eventPrcie" type="text" />
+        ><input id="eventPrcie" type="text" v-model="eventPrice" />
       </div>
       <div class="Input-Box" style="width: 100%">
         <label for="eventRemark">描述</label
-        ><textarea id="eventRemark" cols="30" rows="10"></textarea>
+        ><textarea
+          id="eventRemark"
+          cols="30"
+          rows="10"
+          v-model="eventRemark"
+        ></textarea>
       </div>
 
-      <button type="button">新增</button>
+      <button type="button" @click="setEventDetail()">新增</button>
     </div>
-
-    <h1>{{ Type }}</h1>
-    <h1>{{ Name }}</h1>
-    <h1>{{ Price }}</h1>
+    <div>
+      <div class="list-date-wrapper">
+        <div class="list-date" v-for="{ Date } in eventDetail">
+          <div @click="getEventDetail(Date)">{{ Date }}</div>
+        </div>
+      </div>
+      <div class="list-detail-wrapper">
+        <div
+          class="list-detail"
+          v-for="{
+            EventTime,
+            ConsumeType,
+            PayType,
+            Price,
+            Remake,
+          } in eventDateDetail"
+        >
+          <div class="item-time" :class="'type-' + ConsumeType">
+            {{ EventTime }}
+          </div>
+          <div>
+            <h2>花費方式：{{ PayType }}</h2>
+            <h2>金額：{{ Price }}</h2>
+            <h2>描述：{{ Remake }}</h2>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -88,5 +137,74 @@ button {
   font-weight: 700;
   letter-spacing: 2px;
   border: none;
+}
+
+.list-date-wrapper {
+  width: 100%;
+  height: 100%;
+  margin-top: 1.5rem;
+  padding: 2.5rem 1.5rem;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.list-date {
+  width: 50px;
+  height: 50px;
+  position: relative;
+  color: #ececec;
+  border: none;
+  background-color: #262626;
+  border-radius: 25px;
+  font-size: 2rem;
+  text-align: center;
+  cursor: pointer;
+}
+.list-date:hover {
+  box-shadow: 1px 1px 5px rgba(185, 169, 169, 0.541),
+    -1px -1px 5px rgba(185, 169, 169, 0.541);
+}
+.list-date > div {
+  position: absolute;
+  top: -2px;
+  left: 8px;
+}
+.list-detail-wrapper {
+  width: 100%;
+  height: 100%;
+  background-color: #f9f9f9;
+  color: #363636;
+  margin-top: 1.5rem;
+  padding: 2.5rem 1.5rem;
+}
+.list-detail {
+  display: flex;
+  margin-top: 1.5rem;
+}
+.list-detail:first-child {
+  margin-top: 0;
+}
+.item-time {
+  padding-right: 10px;
+  margin-right: 10px;
+  display: flex;
+  align-items: center;
+  font-size: 1.5rem;
+  border-right: 5px solid #000;
+}
+.type-ENT {
+  border-right: 5px solid rgb(207, 24, 24);
+}
+.type-MED {
+  border-right: 5px solid rgb(61, 207, 24);
+}
+.type-FAD {
+  border-right: 5px solid rgb(207, 112, 24);
+}
+.type-PTF {
+  border-right: 5px solid rgb(248, 212, 9);
+}
+.type-TRA {
+  border-right: 5px solid rgb(28, 176, 221);
 }
 </style>
--- src/stores/events.js
+++ src/stores/events.js
@@ -3,28 +3,133 @@ import { defineStore } from "pinia";
 import { computed, ref } from "vue";
 
 export const useEventStore = defineStore("event", () => {
+  // 消費日期
+  const eventDate = ref("");
   // 消費種類
   const eventConsumeType = ref("");
   // 花費方式
   const eventPayType = ref("");
   // 花費金額
-  const eventPrice = ref(0);
+  const eventPrice = ref("");
   // 花費描述
   const eventRemark = ref("");
+  // 各別日期細項
+  const eventDateDetail = ref([]);
   // 所有細項
-  const eventDetail = ref([
-    { Type: String, Name: String, Price: Number, Remake: String },
+  let eventDetail = ref([
+    {
+      Date: "25",
+      Detail: [
+        {
+          EventTime: timeFormat(),
+          ConsumeType: "ENT",
+          PayType: "信用卡",
+          Price: "$123,123,123.00",
+          Remake: "123123",
+        },{
+          EventTime: timeFormat(),
+          ConsumeType: "MED",
+          PayType: "信用卡",
+          Price: "$123,123,123.00",
+          Remake: "123123",
+        },
+      ],
+    },
+    {
+      Date: "24",
+      Detail: [
+        {
+          EventTime: timeFormat(),
+          ConsumeType: "MED",
+          PayType: "現金",
+          Price: "$123,123,123.00",
+          Remake: "123123",
+        },
+      ],
+    },
   ]);
 
-  function setEventDeatil() {
+  const eventPriceFormat = computed(() => moneyFormat.format(eventPrice.value));
+
+  function setEventDetail() {
     let detail = {
-      Type: eventConsumeType.value,
-      Name: eventPayType.value,
-      Price: eventPrice.value,
-      Remake: eventRemark.value,
+      Date: new Date(eventDate.value).getDate().toString(),
+      Detail: [
+        {
+          EventTime: timeFormat(),
+          ConsumeType: eventConsumeType.value,
+          PayType: eventPayType.value,
+          Price: eventPriceFormat.value,
+          Remake: eventRemark.value,
+        },
+      ],
     };
-    eventDetail.push(detail);
+    if (
+      eventDetail.value.some(
+        (m) => m.Date === new Date(eventDate.value).getDate().toString()
+      )
+    ) {
+      setEvent(eventDetail.value, detail);
+    } else {
+      eventDetail.value = [...eventDetail.value, detail];
+    }
+
+    getEventDetail(new Date(eventDate.value).getDate().toString());
   }
 
-  return { eventDetail, setEventDeatil };
+  function setEvent(eventArray, newEvent) {
+    eventArray.map((eventArray) => {
+      if (eventArray.Date === newEvent.Date) {
+        eventArray.Detail = eventArray.Detail.concat(newEvent.Detail);
+      }
+    });
+  }
+
+  function getEventDetail(date) {
+    eventDateDetail.value = [];
+    eventDetail.value.map((eventArray) => {
+      if (eventArray.Date === date) {
+        eventDateDetail.value = eventDateDetail.value.concat(JSON.parse(JSON.stringify(eventArray.Detail)));
+      }
+    });
+  }
+
+  function timeFormat(){
+    return new Date().getHours().toString().padStart(2, '0') + ":" + new Date().getMinutes().toString().padStart(2, '0')
+  } 
+
+  
+  // 選擇消費種類
+  const selectConsumeType = ref([
+    { val: "FAD", type: "飲食" },
+    { val: "ENT", type: "娛樂" },
+    { val: "MED", type: "醫療" },
+    { val: "PTF", type: "繳費" },
+    { val: "TRA", type: "交通" },
+  ]);
+  // 選擇花費方式
+  const selectPayType = ref([
+    { val: "Cash", type: "現金" },
+    { val: "CreditCard", type: "信用卡" },
+    { val: "EasyCard", type: "悠遊卡" },
+  ]);
+
+  const moneyFormat = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+  });
+
+  return {
+    eventDetail,
+    eventDate,
+    eventConsumeType,
+    eventPayType,
+    eventPrice,
+    eventRemark,
+    eventDateDetail,
+    selectConsumeType,
+    selectPayType,
+    setEventDetail,
+    getEventDetail,
+  };
 });
